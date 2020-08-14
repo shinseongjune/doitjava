16ea9a6a866717b240bbd3623e40344e06e13eb0
shinseongjune <shinseongjune.dev@gmail.com>
2020-08-15 06:04:10 +0900

    Home - 변수 연습

---
+++ doitjava/src/home_practice/VarPractice.java
@@ -0,0 +1,22 @@
+package home_practice;
+
+public class VarPractice {
+
+	public static void main(String[] args) {
+//		var bNum; //자바10부터
+		int bNum;
+		bNum = 1;
+//		bNum = "string";
+		System.out.println(bNum);
+		
+		byte aNum = 1;
+		byte dNum = 2;
+		byte cNum;
+		cNum = (byte)(aNum + dNum);
+		System.out.println(cNum);
+		
+		
+		
+	}
+
+}
