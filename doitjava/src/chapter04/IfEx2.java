58a4773ca2a83ac538e11a9d451f667eb51fea24
shinseongjune <shinseongjune.dev@gmail.com>
2020-08-19 15:57:44 +0900

    Chapter04 - switch-case문

--- doitjava/src/chapter04/IfEx2.java
+++ doitjava/src/chapter04/IfEx2.java
@@ -3,7 +3,7 @@ package chapter04;
 public class IfEx2 {
 
 	public static void main(String[] args) {
-		int score = 70;
+		int score = 75;
 		
 		if (score >= 90) {
 			System.out.println("점수가 90보다 크거나 같습니다.");
@@ -12,8 +12,13 @@ public class IfEx2 {
 			System.out.println("점수가 80보다 크거나 같습니다");
 			System.out.println("등급은 B입니다.");
 		} else if  (score >= 70) {
-			System.out.println("점수가 70보다 크거나 같습니다");
-			System.out.println("등급은 C입니다.");
+			if (score >= 75) {
+				System.out.println("점수가 75보다 크거나 같습니다.");
+				System.out.println("등급은 C+입니다.");
+			} else {
+				System.out.println("점수가 70보다 크거나 같습니다.");
+				System.out.println("등급은 C입니다.");
+			}
 		} else {
 			System.out.println("등급은 F입니다.");
 		}
