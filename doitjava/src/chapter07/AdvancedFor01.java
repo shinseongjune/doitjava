229bd3dc6f8dc1b08c36f225a7b2dd765fb233f6
shinseongjune <shinseongjune.dev@gmail.com>
2020-09-07 15:23:05 +0900

    Chapter07 - 향상된 for문 추가

--- doitjava/src/chapter07/AdvancedFor01.java
+++ doitjava/src/chapter07/AdvancedFor01.java
@@ -5,12 +5,39 @@ public class AdvancedFor01 {
 	public static void main(String[] args) {
 		int[] arr = new int[] {10, 20, 30, 40, 50};
 		for(int i=0; i<arr.length;i++) {
-			System.out.print(arr[i] + " ");
+			System.out.print("(" + i + ")" + arr[i] + " ");
 		}
 		System.out.println();
 		
+		int index = 0;
 		for(int num:arr) {
-			System.out.print(num + " ");
+			System.out.print("(" + index++ + ")" + num + " ");
+		}
+		System.out.println();
+		System.out.println();
+		
+		int[][] ary = {{10,20,30},{40,50,60}};
+		for(int i = 0; i<ary.length;i++) {
+			for(int j = 0;j<ary[i].length;j++) {
+				System.out.print(ary[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(int[] a:ary) {
+			for(int n:a) {
+				System.out.print(n + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(int[] a:ary) { //자바에서는 Advanced for라고 하고 C언어에서는 Range-Based for라고 합니다.
+			for(int i = 0;i<a.length;i++) {
+				System.out.print(a[i] + " ");
+			}
+			System.out.println();
 		}
 		System.out.println();
 	}
