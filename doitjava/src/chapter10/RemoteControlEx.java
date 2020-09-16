df52ac82bf41c9840fb2fd18e9428eef45e29409
shinseongjune <shinseongjune.dev@gmail.com>
2020-09-16 14:57:49 +0900

    Chapter10 - 인터페이스, 구현, 익명 객체, 다중 구현

---
+++ doitjava/src/chapter10/RemoteControlEx.java
@@ -0,0 +1,17 @@
+package chapter10;
+
+public class RemoteControlEx {
+
+	public static void main(String[] args) {
+		RemoteControl rc = null;
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.turnOff();
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.turnOff();
+	}
+
+}
---
+++ doitjava/src/chapter10/RemoteControllerEx.java
@@ -0,0 +1,11 @@
+package chapter10;
+
+public class RemoteControllerEx {
+
+	public static void main(String[] args) {
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+
+}
