e76be57e3cd3d3a29609017eda0e3beb43e6f1dc
shinseongjune <shinseongjune.dev@gmail.com>
2020-09-21 14:00:03 +0900

    Chapter12 - 제네릭, 제네릭 메소드, 와일드카드 타입

--- doitjava/src/chapter12/Util.java
+++ doitjava/src/chapter12/Util.java
@@ -12,4 +12,11 @@ public class Util {
 		boolean valueCompare = p1.getValue().equals(p2.getValue());
 		return keyCompare && valueCompare;
 	}
+	public static <T extends Number> int compare (T t1, T t2) {
+		double v1 = t1.doubleValue();
+		// System.out.println(t1.getClass().getName());
+		double v2 = t2.doubleValue();
+		// System.out.println(t2.getClass().getName());
+		return Double.compare(v1, v2);
+	}
 }
