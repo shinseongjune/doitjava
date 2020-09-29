e0de6789424300f583bf1883d176eaf2c280ee31
shinseongjune <shinseongjune.dev@gmail.com>
2020-09-29 14:27:09 +0900

    Chapter13 - 스트림

--- doitjava/src/chapter13/stream/Student.java
+++ doitjava/src/chapter13/stream/Student.java
@@ -1,6 +1,6 @@
 package chapter13.stream;
 
-public class Student {
+public class Student implements Comparable<Student> {
 	private String name;
 	private int score;
 	
@@ -20,5 +20,10 @@ public class Student {
 	@Override
 	public String toString() {
 		return name + "-" + score;
+	}
+	
+	@Override
+	public int compareTo(Student o) {
+		return Integer.compare(score,  o.score);
 	}
 }
