799e417d3933f46ce50f2ad7afbe3a85142b64e2
shinseongjune <shinseongjune.dev@gmail.com>
2020-09-30 17:33:06 +0900

    1

---
+++ doitjava/src/game/Counting.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Counting {
+	int cnt=1;
+	
+	Counting() {
+		cnt++;
+		if(cnt==12) {
+			System.out.println("¦£+-+-+-+-+¦¤");
+			System.out.println("+Game Over+");
+			System.out.println("¦¦+-+-+-+-+¦¥");
+			break;
+		}
+	}
+}
--- doitjava/src/game/Play.java
+++ doitjava/src/game/Play.java
@@ -7,7 +7,6 @@ public class Play {
 		UserInput us = new UserInput();
 		
 		t.makeTarget();
-		int cnt=1;
 		
 		while(true) {
 			us.input();
@@ -15,18 +14,14 @@ public class Play {
 			
 			if(comp.strike==3) {
 				System.out.println("정답은 [" + t.target[0] + ", " + t.target[1] + ", " + t.target[2] + "] 입니다.");
-				System.out.println(cnt + "회만에 승리했습니다!");
+				System.out.println(c.cnt + "회만에 승리했습니다!");
 				break;
+			Counting c = new Counting();
+			if(c.cnt == 12) {
+				System.out.println("정답은 [" + t.target[0] + ", " + t.target[1] + ", " + t.target[2] + "] 입니다.");
+			}
 			}
 			System.out.println(comp.strike + " 스트라이크, " + comp.ball + " 볼");
-			cnt++;
-			if(cnt==12) {
-				System.out.println("┌+-+-+-+-+┐");
-				System.out.println("+Game Over+");
-				System.out.println("└+-+-+-+-+┘");
-				System.out.println("정답은 [" + t.target[0] + ", " + t.target[1] + ", " + t.target[2] + "] 입니다.");
-				break;
-			}
 		}
 		us.cl();
 	}
