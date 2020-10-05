75a8a6831cbb4b1d65c0914ee7794f3afd7846f4
shinseongjune <shinseongjune.dev@gmail.com>
2020-10-05 13:31:48 +0900

    Chapter14 - try with resources

--- doitjava/src/chapter14/AutoCloseTest.java
+++ doitjava/src/chapter14/AutoCloseTest.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class AutoCloseTest {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		try(AutoCloseObj obj = new AutoCloseObj()){
+		try(
+				AutoCloseObj obj = new AutoCloseObj();
+				Scanner sc = new Scanner(System.in);
+				){
 			int[] arr = new int[3];
 			arr[3] = 100;
 		} catch(Exception e) {
