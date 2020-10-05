c407bb8995a05ecf67a5fdc8e41179862900c14c
shinseongjune <shinseongjune.dev@gmail.com>
2020-10-05 13:29:21 +0900

    Chapter14 - try with resources

---
+++ doitjava/src/chapter14/AutoCloseTest.java
@@ -0,0 +1,15 @@
+package chapter14;
+
+import java.util.Scanner;
+
+public class AutoCloseTest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try(AutoCloseObj obj = new AutoCloseObj()){
+			int[] arr = new int[3];
+			arr[3] = 100;
+		} catch(Exception e) {
+			System.out.println("예외 부분입니다.");
+		}
+	}
+}
